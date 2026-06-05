6981eed711760383ccb23837724b535263590871
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-06-05 14:30:44 +0530

    mongo db connect

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const {DB_NAME} = "../constants.js"
+
+
+
+const connectDB = async () => {
+    try {
+       const connectionInstance = await mongoose.connect
+       (`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log(`mongoDB connected successfully !! DB HOST :${connectionInstance.connection.host}`)
+       
+        
+    } catch (error) {
+        console.log("DataBse connection failed error :" , error);
+        process.exit(1);
+    }
+
+}
+export default connectDB ;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path : "./env"
+});
+ 
+
+connectDB();
+
+
